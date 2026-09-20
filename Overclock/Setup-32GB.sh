798eab89c4bf550188397ecbc2cb98fc9a08bd6b
Forbidden Darkness <ny.forbiddendarkness@gmail.com>
2026-09-20 02:59:41 -0700

    Update Setup-32GB.sh

--- Overclock/Setup-32GB.sh
+++ Overclock/Setup-32GB.sh
@@ -59,6 +59,7 @@ echo "[●] Step 5/8: Injecting optimized performance flags into atomic kernel a
 (rpm-ostree kargs --delete=zram.num_devices=1 2>/dev/null || true) &>/dev/null
 (rpm-ostree kargs --delete=zswap.max_pool_percent=25 2>/dev/null || true) &>/dev/null
 (rpm-ostree kargs --delete=zswap.compressor=lz4 2>/dev/null || true) &>/dev/null
+(rpm-ostree kargs --delete=zswap.compressor=zstd 2>/dev/null || true) &>/dev/null
 
 (rpm-ostree kargs --append-if-missing=mitigations=off 2>/dev/null || true) &>/dev/null
 (rpm-ostree kargs --append-if-missing=zswap.enabled=1 2>/dev/null || true) &>/dev/null
